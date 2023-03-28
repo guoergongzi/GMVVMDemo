f904f56ea91b7c8f497afb8e3f0fb02a87d38e94
guoergongzi <17602232027@163.com>
2023-03-28 10:28:45 +0800

    添加注释

--- gdatabindingdemo4/src/main/java/com/gegz/gdatabindingdemo4/MainActivity.java
+++ gdatabindingdemo4/src/main/java/com/gegz/gdatabindingdemo4/MainActivity.java
@@ -11,18 +11,21 @@ import com.gegz.gdatabindingdemo4.databinding.ActivityMainBinding;
 
 import javax.net.ssl.HttpsURLConnection;
 
+/**
+ * Demo主界面
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMainBinding mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-
+        // 测试自定义属性
         mainBinding.setImageUrl("https://profile.csdnimg.cn/C/B/6/1_wccadab");
-
+        // 测试自定义属性组合
         mainBinding.setViewHeight(200);
         mainBinding.setViewWidth(300);
-
+        // 测试自定义属性覆盖系统属性
         mainBinding.setTextContent("测试");
     }
 }
